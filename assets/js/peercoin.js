3390180e0f5542d09e6141798e0f757b4a590304
Maxim Pogorelov <pogorelovm23@gmail.com>
2017-09-08 10:39:38 +0300

    formatted

--- assets/js/peercoin.js
+++ assets/js/peercoin.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($) {
 
-	$('a.smoothScroll').click(function(){
-	    $('html, body').animate({
-	        scrollTop: $( $.attr(this, 'href') ).offset().top
-	    }, 500);
-	    return false;
+	$('a.smoothScroll').click(function() {
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 500);
+		return false;
 	});
 	$('#lang-tooltip').tooltip()
 });
